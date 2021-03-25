705f58ca643005e3c6e5f9085ccd910ce3b63290
Serhii Olshanskyi <seven8959@gmail.com>
2021-03-25 18:56:03 +0200

    feat: add task transform-arrays

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,49 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("ERROR");
+  }
+
+  if (
+    arr.find(
+      (el) =>
+        el === "--discard-next" ||
+        el === "--discard-prev" ||
+        el === "--double-next" ||
+        el === "--double-prev"
+    ) === undefined
+  ) {
+    return arr;
+  }
+
+  let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case "--discard-next":
+        i++;
+        break;
+      case "--discard-prev":
+        if (arr[i - 2] !== "--discard-next") {
+          result.pop();
+        }
+        break;
+      case "--double-next":
+        if (arr[i + 1] !== undefined) {
+          result.push(arr[i + 1]);
+        }
+        break;
+      case "--double-prev":
+        if (arr[i - 1] !== undefined && arr[i - 2] !== "--discard-next") {
+          result.push(arr[i - 1]);
+        }
+        break;
+      default:
+        result.push(arr[i]);
+        break;
+    }
+  }
+
+  return result;
 };
