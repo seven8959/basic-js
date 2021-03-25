8300485b625bb3d45c7e8cb0ed5ab0151f74dea7
Serhii Olshanskyi <seven8959@gmail.com>
2021-03-25 19:53:23 +0200

    feat: add task extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,49 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  str = str + "";
+  if (options === undefined) {
+    return str;
+  }
+
+  let repeatTimes = options.repeatTimes;
+  let separator = options.separator;
+  let addition = options.addition;
+  let additionRepeatTimes = options.additionRepeatTimes;
+  let additionSeparator = options.additionSeparator;
+
+  if (repeatTimes === undefined || !Number.isInteger(repeatTimes)) {
+    repeatTimes = 1;
+  }
+
+  if (separator === undefined) {
+    separator = "+";
+  }
+
+  if (addition === undefined) {
+    addition = "";
+  } else {
+    addition = addition;
+  }
+
+  if (
+    additionRepeatTimes === undefined ||
+    !Number.isInteger(additionRepeatTimes)
+  ) {
+    additionRepeatTimes = 1;
+  }
+
+  if (additionSeparator === undefined) {
+    additionSeparator = "|";
+  }
+
+  function repeat(str, count, separator) {
+    return str + (separator + str).repeat(count - 1);
+  }
+
+  return repeat(
+    str + repeat(addition, additionRepeatTimes, additionSeparator),
+    repeatTimes,
+    separator
+  );
 };
-  
